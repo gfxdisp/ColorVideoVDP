79baab6953c193d98a62c25688108701d4b6674e
param <param.catchchaos@gmail.com>
2023-02-17 12:27:08 +0000

    Added MLP masking model

--- pycvvdp/cvvdp_nn_metric.py
+++ pycvvdp/cvvdp_nn_metric.py
@@ -1,31 +1,46 @@
 from pycvvdp import cvvdp
 import torch
+from torchvision.ops import MLP
+
+def load_ckpt(ckpt_path, net):
+    # Load network weights
+    if ckpt_path is not None:
+        checkpoint = torch.load(ckpt_path)
+        net.load_state_dict(checkpoint['state_dict'])
+    return net
 
 """
 ColourVideoVDP metric. Refer to pytorch_examples for examples on how to use this class. 
 """
-class cvvdp_rnn(cvvdp):
-    input_dims = 36     # 9 bands x 4 bands per channel
-    rho_dims = 1        # Condition on base rho band
+class cvvdp_nn(cvvdp):
+    input_dims_masking = 6      # T, R, S, T*S, R*S, abs(T-R)*S
+    input_dims_pooling = 36     # 9 bands x 4 bands per channel
+    rho_dims = 1                # Condition on base rho band
 
     def __init__(self, display_name="standard_4k", display_photometry=None, display_geometry=None, color_space="sRGB", foveated=False, heatmap=None, quiet=False, device=None, temp_padding="replicate", use_checkpoints=False,
-                 hidden_dims=8, num_layers=2, dropout=0.2, pooling='gru', pretrained_net=None):
+                 hidden_dims=8, num_layers=2, dropout=0.2, masking='base', masking_ckpt=None, pooling='base', pooling_ckpt=None):
         super().__init__(display_name, display_photometry, display_geometry, color_space, foveated, heatmap, quiet, device, temp_padding, use_checkpoints)
+
+        assert masking in ('base', 'mlp')
+        self.masking = masking
+        if masking == 'mlp':
+            # Separate args (hidden_dims, dropout, num_layers, etc) for masking net
+            self.masking_net = MLP(self.input_dims_masking, [hidden_dims]*num_layers + [1], activation_layer=torch.nn.ReLU, dropout=dropout)
+            self.masking_net = load_ckpt(masking_ckpt, self.masking_net)
+            self.masking_net.to(self.device)
+            self.masking_net.eval()
+
         assert pooling in ('base', 'lstm', 'gru')
         self.pooling = pooling
         rnn_type = {'lstm': torch.nn.LSTM, 'gru': torch.nn.GRU}
         if pooling in ('lstm', 'gru'):
-            recurrent_net = rnn_type[pooling](self.input_dims, hidden_dims, num_layers, dropout=dropout)
+            recurrent_net = rnn_type[pooling](self.input_dims_pooling, hidden_dims, num_layers, dropout=dropout)
             linear = torch.nn.Sequential(
                 torch.nn.Linear(hidden_dims + self.rho_dims, 1),   # rho_band appended here
                 torch.nn.Sigmoid()
             )
             self.pooling_net = torch.nn.Sequential(recurrent_net, linear)
-
-            # Load network weights
-            if pretrained_net is not None:
-                checkpoint = torch.load(pretrained_net)
-                self.pooling_net.load_state_dict(checkpoint['state_dict'])
+            self.pooling_net = load_ckpt(pooling_ckpt, self.pooling_net)
 
             self.pooling_net.to(self.device)
             self.pooling_net.eval()
@@ -41,6 +56,18 @@ class cvvdp_rnn(cvvdp):
 
             stats = {}
             return (Q_jod.squeeze(), stats)
+
+    def apply_masking_model(self, T, R, S):
+        if self.masking == 'base':
+            return super().apply_masking_model(T, R, S)
+        else:
+            c, _, h, w = T.shape
+            if S.dim() == 0:
+                S = torch.full_like(T, S)
+            T, R, S = T.flatten(), R.flatten(), S.flatten()
+            feat_in = torch.stack((T, R, S, T*S, R*S, torch.abs(T - R)*S), dim=-1)
+            D = self.masking_net(feat_in).reshape(c, 1, h, w)
+            return D
 
     # Perform pooling with per-band weights and map to JODs
     def do_pooling_and_jods(self, Q_per_ch, base_rho_band):
@@ -67,3 +94,15 @@ class cvvdp_rnn(cvvdp):
             standard_str = ''
         fv_mode = 'foveated' if self.foveated else 'non-foveated'
         return '"ColourVideoVDP with {} v{}, {:.4g} [pix/deg], Lpeak={:.5g}, Lblack={:.4g} [cd/m^2], {}{}"'.format(self.pooling, self.version, self.pix_per_deg, self.display_photometry.get_peak_luminance(), self.display_photometry.get_black_level(), fv_mode, standard_str)
+
+    def train(self):
+        if self.masking == 'mlp':
+            self.masking_net.train()
+        if self.pooling in ('lstm', 'gru'):
+            self.pooling_net.train()
+
+    def eval(self):
+        if self.masking == 'mlp':
+            self.masking_net.eval()
+        if self.pooling in ('lstm', 'gru'):
+            self.pooling_net.eval()
