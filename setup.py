b8b5ecb91a0dcc590025bc0a157f57b066144f2b
Param Hanji <paramhanji@github.com>
2023-03-24 20:47:22 +0000

    Added pandas dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,10 @@ setup(
                       'ffmpeg-python>=0.2.0',
                       'torch>=1.8.2',
                       'ffmpeg>=1.4',
-                      'imageio>=2.19.5'],
+                      'imageio>=2.19.5',
+                      'pandas>=1.5.1',
+                      'tqdm>=4.64.1'
+                     ],
 
     classifiers=[
         'Development Status :: 3 - Alpha',
