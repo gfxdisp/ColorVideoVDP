3442ba0fd9cdb224e84f4901b4c68b96a63fc944
Rafal Mantiuk <mantiuk@gmail.com>
2023-12-20 11:52:27 +0000

    Fixed for the new interface

--- test_on_LIVEHDR.py
+++ test_on_LIVEHDR.py
@@ -8,24 +8,25 @@ import logging
 
 import pycvvdp.utils
 
-display_name = 'samsung_q90t_qled_livehdr_dark'
+display_name = 'samsung_q90t_qled_livehdr_bright'
 
 media_folder = '../datasets/LIVEHDR'
 # TST_FILEs = glob.glob(os.path.join(media_folder, 'train', '1080p_6M_football4.mp4'))
 # ref_file = os.path.join(media_folder, 'train', '4k_ref_football4.mp4')
 
-TST_FILEs = glob.glob(os.path.join(media_folder, 'train', '1080p_1M_UrbanLandmark.mp4'))
-ref_file = os.path.join(media_folder, 'train', '4k_ref_UrbanLandmark.mp4')
+TST_FILEs = glob.glob(os.path.join(media_folder, 'train', '4k_15M_NighTraffic.mp4'))
+ref_file = os.path.join(media_folder, 'train', '4k_ref_NighTraffic.mp4')
 
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
 
-pycvvdp.utils.config_files.set_config_dir(media_folder)
-cvvdp = pycvvdp.cvvdp(display_name=display_name)
+config_paths = [media_folder, "../metric_configs/cvvdp_grad_trans_multbase_tmask/cvvdp_parameters.json"]
+
+cvvdp = pycvvdp.cvvdp(display_name=display_name, config_paths=config_paths)
 cvvdp.debug = True
 
 for tst_fname in TST_FILEs:
 
-    vs = pycvvdp.video_source_file( tst_fname, ref_file, display_photometry=display_name, frames=30, full_screen_resize='bicubic', resize_resolution=(3840, 2160) )
+    vs = pycvvdp.video_source_file( tst_fname, ref_file, display_photometry=display_name, frames=30, full_screen_resize='bicubic', resize_resolution=(3840, 2160), config_paths=config_paths )
 
     start = time.time()
     Q_JOD_static, stats_static = cvvdp.predict_video_source( vs )
