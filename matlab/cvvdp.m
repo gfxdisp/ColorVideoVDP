43998d78946c5201d29b242bc046ae8ce0c38d6c
Rafal Mantiuk <mantiuk@gmail.com>
2025-03-17 11:32:55 +0000

    Updated ChangeLog + small improvements

--- matlab/cvvdp.m
+++ matlab/cvvdp.m
@@ -97,18 +97,13 @@ classdef cvvdp
                 if options.verbose
                     fwrite( 2, cmdout );
                 end
-                jod_res = regexp( cmdout, "cvvdp=[\d\.]*", "match" );
-                
-                % if jod is negative, NaN will be returned, modify the search pattern
-                if length(jod_res{1})<7 
-                    jod_res = regexp( cmdout, "cvvdp=-[\d\.]*", "match" );
-                end
-                
+                jod_res = regexp( cmdout, "cvvdp=-?[\d\.]*", "match" );
+                                
                 jod = str2double(jod_res{1}(7:end));
             end
 
             if ~strcmp(options.heatmap, 'none')
-                heatmap_fn = [ test_file(1:(end-4)), '_heatmap.png' ];
+                heatmap_fn_png = [ test_file(1:(end-4)), '_heatmap.png' ];
                 heatmap_fn_mp4 = [ test_file(1:(end-4)), '_heatmap.mp4' ];
 
                 if ~isfile( heatmap_fn_png ) && ~isfile( heatmap_fn_mp4 )
@@ -119,10 +114,10 @@ classdef cvvdp
                     delete( heatmap_fn_png );
                 elseif isfile( heatmap_fn_mp4 )
                     v_temp = VideoReader( heatmap_fn_mp4 );
-                    heatmap = [];
+                    heatmap = zeros(v_temp.Height, v_temp.Width, 3, v_temp.NumFrames, 'single');
                     frame_count = 1;
                     while hasFrame(v_temp)
-                        raw_frame = double(readFrame(v_temp))/255;
+                        raw_frame = single(readFrame(v_temp))/255;
                         heatmap(:, :, :, frame_count) = raw_frame;
                         frame_count = frame_count + 1;
                     end                    
