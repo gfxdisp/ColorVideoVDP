e931c0b7943c8d7438209d549511004958476d6a
Dounia Hammou <dh706@cl.cam.ac.uk>
2025-04-28 12:12:18 +0100

    Necessary for preparing docker submission

--- pycvvdp/__init__.py
+++ pycvvdp/__init__.py
@@ -4,4 +4,4 @@ from pycvvdp.pupsnr import pu_psnr_y, pu_psnr_rgb2020, psnr_rgb
 from pycvvdp.video_source_file import video_source_file, video_source_video_file, load_image_as_array
 from pycvvdp.display_model import vvdp_display_photometry, vvdp_display_photo_eotf, vvdp_display_geometry
 from pycvvdp.video_source_yuv import video_source_yuv_file
-from pycvvdp.cvvdp_ml_metric import cvvdp_ml
+from pycvvdp.cvvdp_ml_metric import *
